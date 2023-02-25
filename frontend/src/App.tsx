fc21f0d93c82e279c077f81ef73d377c77727103
hkrobotics <kumarhemant24jan@gmail.com>
2023-02-25 14:58:16 +0530

    config : global style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./Store/Reducers/TestReducer";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './Store/Reducers/TestReducer';
 import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { router } from "./routes";
+	createBrowserRouter,
+	RouterProvider,
+} from 'react-router-dom';
+import { router } from './routes';
+import GlobalStyle from './globalStyles';
 
 function App() {
 
-  const counter: number = useSelector((state: any) => state.test.counter);
-  const dispatch: any = useDispatch();
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+	const counter: number = useSelector((state: any) => state.test.counter);
+	const dispatch: any = useDispatch();
+	return (
+		<>
+			<GlobalStyle />
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
