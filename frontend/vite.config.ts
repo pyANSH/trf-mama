03733f832942bc4bcd94f32279618bb523e6988a
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-02-25 16:45:29 +0530

    change host port to 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
-import checker from "vite-plugin-checker";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
+	build: {
+		outDir: 'build',
+	},
+	server:{
+		host:'localhost',
+		port:3000
+	},
 
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-    }),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        svgoConfig: {
-          plugins: [
-            {
-              removeViewBox: false,
-            },
-          ],
-        },
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-  ],
+	plugins: [
+		react(),
+		checker({
+			typescript: true,
+		}),
+		svgrPlugin({
+			svgrOptions: {
+				icon: true,
+				svgoConfig: {
+					plugins: [
+						{
+							removeViewBox: false,
+						},
+					],
+				},
+				// ...svgr options (https://react-svgr.com/docs/options/)
+			},
+		}),
+	],
 });
