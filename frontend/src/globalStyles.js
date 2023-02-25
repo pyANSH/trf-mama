493fcf339e5da401c674587aa6e53f9aea7f7d42
hkrobotics <kumarhemant24jan@gmail.com>
2023-02-25 16:13:21 +0530

    fix : globalStyles

--- frontend/src/globalStyles.js
+++ frontend/src/globalStyles.js
@@ -1,13 +1,19 @@
 // globalStyles.js
 import { createGlobalStyle } from 'styled-components';
-
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
-  }
+    html{
+        scroll-behavior: smooth;
+    }
+    body {
+        /* font-family: "Work Sans", sans-serif; */
+        background: #ffffff;
+    }
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: "Work Sans", sans-serif;
+    }
 `;
 
 export default GlobalStyle;
