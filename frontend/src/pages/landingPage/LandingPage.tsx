8c0dab474b3e3f7523b0e57ee6b7d0037502daae
hkrobotics <kumarhemant24jan@gmail.com>
2023-02-27 03:22:28 +0530

    feat : video player and marquee

--- frontend/src/pages/landingPage/LandingPage.tsx
+++ frontend/src/pages/landingPage/LandingPage.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import {useEffect,useState} from 'react';
+import Marquee from 'react-fast-marquee';
+
 
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 200px;
+
   width: 100%;
   min-height: 200vh;
+  background: #dfdbdb;
+
+  align-items: center;
+
+  padding:16px 16px 0px 16px;
 `;
+
+const HeroContainer = styled.div`
+  background: #fafafa;
+  border-radius: 30px;
+  min-height: 100vh;
+  width: 100%;
+  /* margin: 16px; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  /* gap: 184px; */
+`;
+
 
 const Header = styled.header`
   background: transparent;
@@ -24,8 +48,8 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 52px 52px;
-  width: 98%;
+  padding: 52px 68px;
+  width: 100%;
 `;
 
 const LogoText = styled.h1`
@@ -74,6 +98,9 @@ const NavText = styled.p(() => ({
 	fontSize: '16px',
 	lineHeight: '20px',
 	color: '#000',
+	':hover': {
+		color: '#8330c2',
+	}
 }));
 
 const Logo = styled.div`
@@ -87,6 +114,13 @@ const Button = styled.button`
   background: transparent;
   border: none;
   outline: none;
+  background: #8330c2;
+  transition: all 0.3s ease-in-out;
+  :hover {
+    background: #9e4cdc;
+    transition: all 0.3s ease-in-out;
+  }
+  /* background: #9e4cdc; */
 `;
 
 const BTN_Login = styled(Button)`
@@ -99,7 +133,6 @@ const BTN_Login = styled(Button)`
 `;
 
 const BTN_SignUp = styled(Button)`
-  background: #9e4cdc;
   padding: 19px 63px;
   border-radius: 30px;
   color: #fff;
@@ -113,17 +146,6 @@ const BTN_SignUp = styled(Button)`
 //   gap: 20px;
 // `;
 
-const Hero = styled.div`
-  background: #fafafa;
-  border-radius: 30px;
-  height: 100vh;
-  width: 98%;
-  margin: 16px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
 
 const HeroHeadingText = styled.h1`
   font-weight: 500;
@@ -139,7 +161,7 @@ const HeroBTN = styled(BTN_SignUp)`
   font-size: 28px;
   line-height: 33px;
   border-radius: 70px;
-  margin: 56px 0;
+  /* margin: 56px 0; */
 `;
 
 const HeroText = styled.p`
@@ -149,16 +171,58 @@ font-size: 96px;
 line-height: 113px;
 text-align: center;
 
-width: 800px;
+width: 100%;
+max-width: 800px;
 `;
 
+const HeroTextAndBTNContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 56px;
+
+  min-height: 100vh;
+  `;
+
 const VideoContainer = styled.div(() => ({
-	borderRadius:'50%',
 	height: '400px',
 	width: '400px',
-	backgroundColor: 'red',
+	overflow: 'hidden',
+	borderRadius:'50%',
+	marginBlockEnd:'100px',
+	// boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'
+	// backgroundColor: 'red',
 }));
 
+
+const ConnectWithTutorsTextContainer = styled.div`
+  width: 100%;
+  padding: 75px 0px;
+  max-width: 900px;
+  width: 100%;
+
+`;
+
+const ConnectWithTutorsText = styled.p`
+font-style: normal;
+font-weight: 500;
+font-size: 126px;
+line-height: 91.8%;
+
+text-align: center;
+letter-spacing: -0.08em;
+`;
+
+const MarqueeItem = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0px;
+  height: 100px;
+  width: 100px;
+  
+`;
 
 function LandingPage() {
 
@@ -195,36 +259,110 @@ function LandingPage() {
 	function handleLoginClick() {
 		navigate('/onboard');
 	}
+  
+	const MarqueeData = [
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+		{
+			logo:'https://s3-alpha-sig.figma.com/img/351f/cbd5/626a841ccc5a444f33c6037a5f1b67b2?Expires=1678060800&Signature=RwWxSYQ04lLC8M14AKLCuxsPP3uUB4gufSRJ99tlwaXf4Eexsiq8e3vsc8wXrIbImzyJufRbc3NtYnxgX4iPl7MdewBkRdZaccVUjCYcvqLn-6M1p1ChjNsoectqbc0DhUUP4DDNWGCoGDZE0VbWNSBubwSrWPjN9ibQgh-xfCrFqgUZlYIl~4DIjmLwL2Ax2UrxuX-QedwOoVfPO8LnyvVjgQj5ZbP2cbJcZwhKA5OL~Sadrje5nRz2Lf2GsG2t3BHIyNfuCX~qMU6jHW53XTKf9b4fJsxccSy54flitP3r3db0nlxOQYJj8ROMzSClYiLJwz1T2NVbqUEMjI0vRw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+		},
+	];
 
 	return (
 		<MainContainer>
-			<Header>
-				<LogoText>{'mama.'}</LogoText>
+			<HeroContainer>
 
-				<NavContainer>
-					<NavOptionsContainer>
-						{NavItems.map((item) => (
-							<NavItem key={item.id}>
-								<NavText onClick={item.onClick}>{item.text}</NavText>
-							</NavItem>
-						))}
-					</NavOptionsContainer>
-				</NavContainer>
+				<Header>
+					<LogoText onClick={()=>{navigate('/');}}>{'mama.'}</LogoText>
 
-				<BTN_SignUp onClick={handleLoginClick}>{'Log in'}</BTN_SignUp>
+					<NavContainer>
+						<NavOptionsContainer>
+							{NavItems.map((item) => (
+								<NavItem key={item.id}>
+									<NavText onClick={item.onClick}>{item.text}</NavText>
+								</NavItem>
+							))}
+						</NavOptionsContainer>
+					</NavContainer>
 
-			</Header>
+					<BTN_SignUp onClick={handleLoginClick}>{'Log in'}</BTN_SignUp>
 
-			<Hero>
-				<HeroText>
-					{'Personalized learning for you'}
-				</HeroText>
-				<HeroBTN>{'Sign Up for free'}</HeroBTN>
-			</Hero>
+				</Header>
 
-			<VideoContainer>
+			
+				<HeroTextAndBTNContainer>
+					<HeroText>
+						{'Personalized learning for you'}
+					</HeroText>
+					<HeroBTN onClick={handleLoginClick}>{'Start your journey '}</HeroBTN>
+				</HeroTextAndBTNContainer>
+			
+
+				<VideoContainer>
+					<ReactPlayer 
+						url={'https://www.sendpotion.com/assets/video/home/hero/1.mp4?6'}
+						muted={true}
+						playing={true}
+						loop={true}
+						height={'400px'}
+						width={'400px'}
+						controls={false}
+						style={{borderRadius:'50%'}}
+					/>
+				</VideoContainer>
+
+			</HeroContainer>
+
+			<ConnectWithTutorsTextContainer>
+				<ConnectWithTutorsText>
+					{'Connect with tutors across🌏'}
+				</ConnectWithTutorsText>
+			</ConnectWithTutorsTextContainer>
+
+			{/* <UniversityScrollerContainer>
         
-			</VideoContainer>
+			</UniversityScrollerContainer> */}
+
+			<Marquee
+				gradient
+				gradientColor={[255,255,255]}
+				speed={100}
+				style={{
+					display:'flex',
+					flexDirection:'row',
+					gap:'20px',
+				}}
+			>
+				{
+					MarqueeData.map((item,index)=>{
+						return (
+							<MarqueeItem key={index} src={item.logo} />
+						);
+					})
+				}
+			</Marquee>
 
 		</MainContainer>
 	);
