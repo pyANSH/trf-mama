b25154b9a20d64d87c3ffe677578b9fd7295a474
hkrobotics <kumarhemant24jan@gmail.com>
2023-02-27 03:23:03 +0530

    feat : add custom scrollbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,35 @@ import { router } from './routes';
 import GlobalStyle from './globalStyles';
 import { lightThemeColorScheme } from './config/styles';
 import { ThemeProvider } from 'styled-components';
+import { Scrollbars } from 'react-custom-scrollbars';
+
 
 function App() {
 
 	// const counter: number = useSelector((state: any) => state.test.counter);
 	// const dispatch: any = useDispatch();
 	return (
-		<ThemeProvider theme={lightThemeColorScheme}>
-			<GlobalStyle />
-			<RouterProvider router={router} />
-		</ThemeProvider>
+		<Scrollbars 
+			autoHide
+			style={{ width: '100vw', height: '100vh' }}
+			autoHideTimeout={1000}
+			autoHideDuration={200}
+			thumbMinSize={10}
+			renderThumbVertical={({ style, ...props }) =>
+				<div {...props} style={{ 
+					...style, 
+					backgroundColor: 'rgba(0,0,0,.8)',
+					borderRadius: '10px' 
+				}} />
+			}
+		>
+			<ThemeProvider theme={lightThemeColorScheme}>
+			
+				<GlobalStyle />
+				<RouterProvider router={router} />
+			
+			</ThemeProvider>
+		</Scrollbars>
 	);
 }
 
