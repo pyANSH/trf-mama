8dfdde6fe1f091bd1c8f388b0f4aabb79536e942
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2023-02-26 10:27:05 +0530

    fix: conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,20 @@
-<<<<<<< HEAD
-import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-import {RouterProvider} from 'react-router-dom';
-import { router } from './routes';
-import GlobalStyle from './globalStyles';
-import { lightThemeColorScheme } from './config/styles';
-import { ThemeProvider } from 'styled-components';
-
-function App() {
-
-	// const counter: number = useSelector((state: any) => state.test.counter);
-	// const dispatch: any = useDispatch();
-	return (
-		<ThemeProvider theme={lightThemeColorScheme}>
-			<GlobalStyle />
-			<RouterProvider router={router} />
-		</ThemeProvider>
-	);
-=======
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./Store/Reducers/TestReducer";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import { useSelector, useDispatch } from 'react-redux';
+import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import GlobalStyle from "./globalStyles";
+import { lightThemeColorScheme } from "./config/styles";
+import { ThemeProvider } from "styled-components";
 
 function App() {
-
-  const counter: number = useSelector((state: any) => state.test.counter);
-  const dispatch: any = useDispatch();
+  // const counter: number = useSelector((state: any) => state.test.counter);
+  // const dispatch: any = useDispatch();
   return (
-    <>
+    <ThemeProvider theme={lightThemeColorScheme}>
+      <GlobalStyle />
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
->>>>>>> b98104a334bd3aed39126dfc4ce5f29badf97216
 }
 
 export default App;
