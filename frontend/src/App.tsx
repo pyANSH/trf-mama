0d86842f12f68aabc393e8bb6dea21cf97230971
hkrobotics <kumarhemant24jan@gmail.com>
2023-03-01 07:36:50 +0530

    revert : custom-react-scrollbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
-import {RouterProvider} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import GlobalStyle from './globalStyles';
 import { lightThemeColorScheme } from './config/styles';
 import { ThemeProvider } from 'styled-components';
-import { Scrollbars } from 'react-custom-scrollbars';
 
 
 function App() {
@@ -13,27 +12,10 @@ function App() {
 	// const counter: number = useSelector((state: any) => state.test.counter);
 	// const dispatch: any = useDispatch();
 	return (
-		<Scrollbars 
-			autoHide
-			style={{ width: '100vw', height: '100vh' }}
-			autoHideTimeout={1000}
-			autoHideDuration={200}
-			thumbMinSize={10}
-			renderThumbVertical={({ style, ...props }) =>
-				<div {...props} style={{ 
-					...style, 
-					backgroundColor: 'rgba(0,0,0,.8)',
-					borderRadius: '10px' 
-				}} />
-			}
-		>
-			<ThemeProvider theme={lightThemeColorScheme}>
-			
-				<GlobalStyle />
-				<RouterProvider router={router} />
-			
-			</ThemeProvider>
-		</Scrollbars>
+		<ThemeProvider theme={lightThemeColorScheme}>
+			<GlobalStyle />
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	);
 }
 
