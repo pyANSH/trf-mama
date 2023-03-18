0d0070203fca84d67638cea68a67e168db026501
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-03-18 11:42:29 +0530

    add left container to dashboard

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,48 +1,150 @@
-import React from "react";
+import { SquaresFour } from "phosphor-react";
+import {Books} from "phosphor-react";
+import {Coins} from "phosphor-react";
+import {GraduationCap} from "phosphor-react";
+import {User} from "phosphor-react";
+import React, { useState } from "react";
+import styled from "styled-components";
 import "./App.css";
+
+const Container = styled.div`
+
+` 
+
+const Header = styled.div`
+
+`;
+
+const Logo = styled.h1`
+
+`
+
+const LeftContainer =styled.div`
+display: flex;
+flex-direction: column;
+gap:72px;
+padding-left: 80px;
+padding-top: 80px;
+`;
+
+const SingleMenuItem =styled.div`
+display: flex;
+align-items: center;
+gap:20px;
+`;
+
+const SquareFourIcon = styled(SquaresFour)`
+width:26px;
+height:26px;
+color:#7C7C7C;
+`;
+
+const SellIcon =styled(Coins)`
+width:26px;
+height:26px;
+color:#7C7C7C;
+`;
+
+const BooksIcon =styled(Books)`
+width:26px;
+height:26px;
+color:#7C7C7C;
+`;
+
+const CapIcon =styled(GraduationCap)`
+width:26px;
+height:26px;
+color:#7C7C7C;
+`;
+
+const UserIcon =styled(User)`
+width:26px;
+height:26px;
+color:#7C7C7C;
+`;
+
+const OptionText =styled.p(()=>({
+  fontWeight: '500',
+fontSize: '22px',
+lineHeight: '26px',
+}))
+
+const CommonIcons = {
+	height: '24px',
+	width: '24px',
+};
+const SidebarOptionIcon = styled.div(({ }) => ({
+	...CommonIcons,
+}));
+
 function Dashboard() {
+
+const [tab,setTab] =useState(0);
+
+  let sidebarOptions = [
+		{
+			icon: <SquareFourIcon />,
+			text: 'Dashboard',
+			/* component: isWhiteLabel ? <ManageGroupChannels /> : <ManageChannels />, */
+		},
+    {
+			icon: <SellIcon />,
+			text: 'Sell Documents',
+			/* component: isWhiteLabel ? <ManageGroupChannels /> : <ManageChannels />, */
+		},
+    {
+			icon: <BooksIcon />,
+			text: 'The Notebank',
+			/* component: isWhiteLabel ? <ManageGroupChannels /> : <ManageChannels />, */
+		},
+    {
+			icon: <CapIcon />,
+			text: 'Community',
+			/* component: isWhiteLabel ? <ManageGroupChannels /> : <ManageChannels />, */
+		},
+    {
+			icon: <UserIcon />,
+			text: 'Dashboard',
+			/* component: isWhiteLabel ? <ManageGroupChannels /> : <ManageChannels />, */
+		},
+    
+    ];
+
+    function handleSidebarOptionClick(index:any, option:any){
+      
+    }
+
   return (
-    <div className="dashboard">
-      <header className="header">
-        <h1 className="logo">mama.</h1>
-        <div className="nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a href="#" className="nav__link">
-                Why Mama
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#" className="nav__link">
-                The Zone
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#" className="nav__link">
-                Pricing
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#" className="nav__link">
-                FAQs
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="login">
-          <button className="btn__login">Login</button>
-          <button className="btn__signup">Sign Up</button>
-        </div>
-      </header>
-      <div className="hero">
-        <div></div>
-        <div className="hero__heading">
-          <h1>Personalised</h1>
-          <h1>learning for you</h1>
-        </div>
-        <button className="hero__btn btn__signup">Sign Up for free</button>
-      </div>
-    </div>
+    <Container>
+      <Header>
+        <Logo>mama.</Logo>
+      </Header>
+      <LeftContainer>
+      {
+												sidebarOptions.map((option, index) => {
+													if (!option) return null; //? to handle null value in array
+													return (
+														<>
+															<SingleMenuItem
+																key={index}
+																onClick={() => handleSidebarOptionClick(index, option)}
+															>
+															<SidebarOptionIcon>
+																	{option.icon}
+																</SidebarOptionIcon>
+																<OptionText>
+																	{option.text}
+																</OptionText>
+															</SingleMenuItem>
+														</>
+													);
+												})
+											}
+<>
+
+</>
+      </LeftContainer>
+    </Container>
   );
 }
 
