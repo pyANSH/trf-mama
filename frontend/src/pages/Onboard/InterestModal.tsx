0a9db783fe3c1bdc0141f08590310f2fcc552665
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2023-04-22 13:03:55 +0530

    feat: add interest Popup logic

--- frontend/src/pages/Onboard/InterestModal.tsx
+++ frontend/src/pages/Onboard/InterestModal.tsx
@@ -1,203 +1,209 @@
 import { CaretDown, CaretUp, Check, X } from 'phosphor-react';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { appTypography } from '../../config/styles';
 import { useDispatch } from 'react-redux';
 
-
 const Backdrop = styled.div(({ theme }) => ({
-	position: 'fixed',
-	height: '100vh',
-	width: '100vw',
-	top: '50%',
-	left: '50%',
+  position: 'fixed',
+  height: '100vh',
+  width: '100vw',
+  top: '50%',
+  left: '50%',
 
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	transform: 'translate(-50%, -50%)',
-	background: theme.app.neutral['500'],
-	opacity: 0.3,
-	zIndex: 2,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  transform: 'translate(-50%, -50%)',
+  background: theme.app.neutral['500'],
+  opacity: 0.3,
+  zIndex: 2,
+}));
+const BtnConfirm = styled.button(() => ({
+  color: '#fff',
+  background: '#9E4CDC',
+  borderRadius: '16px',
+  padding: '8px 16px',
+  cursor: 'pointer',
+  border: 'none',
+  outline: 'none',
 }));
 
 const ModalMainContainer = styled.div(({ theme }) => ({
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	padding: '24px 30px 14px',
-	gap: '14px',
-	position: 'fixed',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: '420px',
-	background: theme.app.shades.white,
-	boxShadow: '0px -4px 42px rgba(84, 84, 84, 0.03)',
-	borderRadius: '24px',
-	zIndex: 3,
-	'@media (max-width:575px)': {
-		width: '300px'
-	}
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '24px 30px 14px',
+  gap: '14px',
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '420px',
+  background: theme.app.shades.white,
+  boxShadow: '0px -4px 42px rgba(84, 84, 84, 0.03)',
+  borderRadius: '24px',
+  zIndex: 3,
+  '@media (max-width:575px)': {
+    width: '300px',
+  },
 }));
 
-const SingleInterestContainer =styled.div`
+const SingleInterestContainer = styled.div``;
+const SingleInterestText = styled.p(
+  ({ theme, isActive }: { theme: any; isActive: boolean }) => ({
+    ...appTypography.paraMed.regular,
+    color: isActive ? theme.app.shades.white : theme.app.typography['900'],
+    background: isActive ? theme.app.primary['500'] : 'initial',
+    padding: '8px',
+    border: isActive
+      ? `1px solid ${theme.app.shades.white}`
+      : `1px solid ${theme.app.neutral['800']}`,
+    borderRadius: '100px',
+    cursor: 'pointer',
+  }),
+);
 
+const InterstContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
 `;
-const SingleInterestText =styled.p(({theme,isActive}:{theme:any,isActive:boolean})=>({
-...appTypography.paraMed.regular,
-color:isActive?theme.app.shades.white:theme.app.typography['900'],
-background:isActive?theme.app.primary['500']:'initial',
-padding:'8px',
-border:isActive?`1px solid ${theme.app.shades.white}`:`1px solid ${theme.app.neutral['800']}`,
-borderRadius:'100px',
-cursor:'pointer'
-
-}));
-
-const InterstContainer =styled.div`
-display: flex;
-flex-wrap: wrap;
-gap:8px;
-
+const GenderText = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
 `;
-const GenderText=styled.div`
-
-font-weight: 400;
-font-size: 18px;
-line-height: 24px;
-`;
-const DropDownAns=styled.div`
-border: 1px solid grey;
-border-radius: 8px;
-padding: 12px 16px;
-display: flex;
-justify-content: space-between;
-cursor: pointer;
-align-items: center;
+const DropDownAns = styled.div`
+  border: 1px solid grey;
+  border-radius: 8px;
+  padding: 12px 16px;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+  align-items: center;
 `;
 
 const DropDownMainContainer = styled.div`
-width:100%;
-box-shadow: 0px 8px 24px -4px rgba(27, 46, 94, 0.08);
-border-radius: 8px;
-border: 1px solid #E1E3E6;
-padding: 24px 9px;
-display:flex;
-flex-direction: column;
-gap: 8px;
+  width: 100%;
+  box-shadow: 0px 8px 24px -4px rgba(27, 46, 94, 0.08);
+  border-radius: 8px;
+  border: 1px solid #e1e3e6;
+  padding: 24px 9px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
 
-const DropDownOption =styled.p`
-padding: 8px 16px;
-border-radius: 8px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-cursor: pointer;
-
+const DropDownOption = styled.p`
+  padding: 8px 16px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
 `;
 
-const DropDownOptionText =styled.p`
-font-weight: 400;
-font-size: 18px;
-line-height: 24px;
+const DropDownOptionText = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
 `;
 
-const TickIcon=styled(Check)`
-font-weight: 400;
-font-size: 18px;
-line-height: 24px;
+const TickIcon = styled(Check)`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
 `;
 
-const UpArrowIcon=styled(CaretUp)`
-font-weight: 400;
-font-size: 18px;
-line-height: 24px;
+const UpArrowIcon = styled(CaretUp)`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
 `;
-const DownArrowIcon=styled(CaretDown)`
-font-weight: 400;
-font-size: 18px;
-line-height: 24px;
+const DownArrowIcon = styled(CaretDown)`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
 `;
-
-
 
 function InterestModal() {
+  const interestList = [
+    'Education 🎓',
+    'Yeeeah, science! ⚗️',
+    'Art 🎭',
+    'Sport ⚽',
+    'Games 🎮',
+    'Health 🏥',
+  ];
 
-const interestList =[
-	'Education 🎓','Yeeeah, science! ⚗️','Art 🎭','Sport ⚽','Games 🎮','Health 🏥'
-]
+  const [isUserTypeDropdownOpen, setIsUserTypeDropdownOpen] = useState(false);
 
-const [isUserTypeDropdownOpen, setIsUserTypeDropdownOpen] = useState(false);
+  const [userType, setUserType] = useState('Student');
 
-const [userType, setUserType] = useState('Student')
+  const userDropdownList = ['Student', 'Mentor'];
 
-const userDropdownList = ['Student','Mentor']
-
-
-const [interestArr, setInterestArr] = useState<any>([]);
-const dispatch = useDispatch();
-function handleInterestInsert(interestName:string) {
-	if(interestArr.some((interest:string)=>interest===interestName)){
-	const interestIndex=interestArr.findIndex((interest:string)=>interest===interestName);
-	let tempInterstArr =[...interestArr];
-	tempInterstArr.splice(interestIndex,1);
-	setInterestArr(tempInterstArr);
-	}else{
-		if(interestArr.length>2){
-			
-			return;
-		}
-	let tempInterstArr =[...interestArr];
-	tempInterstArr.push(interestName)
-	setInterestArr(tempInterstArr)	
-	}
-	
-}
-
-	return (
+  const [interestArr, setInterestArr] = useState<any>([]);
+  const dispatch = useDispatch();
+  function handleInterestInsert(interestName: string) {
+    if (interestArr.some((interest: string) => interest === interestName)) {
+      const interestIndex = interestArr.findIndex(
+        (interest: string) => interest === interestName,
+      );
+      let tempInterstArr = [...interestArr];
+      tempInterstArr.splice(interestIndex, 1);
+      setInterestArr(tempInterstArr);
+    } else {
+      if (interestArr.length > 2) {
+        return;
+      }
+      let tempInterstArr = [...interestArr];
+      tempInterstArr.push(interestName);
+      setInterestArr(tempInterstArr);
+    }
+  }
+  const handleSubmit = () => {};
+  return (
     <>
-			<Backdrop />
-            <ModalMainContainer>
-				<InterstContainer>
-{interestList.map((interest)=>(
-	
-<SingleInterestText isActive={interestArr.some((interestName:string)=>interestName===interest)} onClick={()=>handleInterestInsert(interest)}>
-{interest}
-</SingleInterestText>
-	))}
-				</InterstContainer>
-
-				<DropDownAns onClick={()=>setIsUserTypeDropdownOpen(!isUserTypeDropdownOpen)}>
-    <GenderText>
-
-{userType}
-    </GenderText>
-   {isUserTypeDropdownOpen?<UpArrowIcon/>:<DownArrowIcon/>}
-</DropDownAns>
-
-{isUserTypeDropdownOpen&&<DropDownMainContainer>
-{userDropdownList.map((feed)=>(
-    <DropDownOption
-     onClick={()=>{
-		setUserType(feed);
-		setIsUserTypeDropdownOpen(false);
-		
-		}}>
-        <DropDownOptionText>
-{feed}
-        </DropDownOptionText>
-        {userType===feed&&<TickIcon/>}
-    </DropDownOption>
-))}
-</DropDownMainContainer>}
-
-
-            </ModalMainContainer>
-    
+      <Backdrop />
+      <ModalMainContainer>
+        <InterstContainer>
+          {interestList.map((interest) => (
+            <SingleInterestText
+              isActive={interestArr.some(
+                (interestName: string) => interestName === interest,
+              )}
+              onClick={() => handleInterestInsert(interest)}
+            >
+              {interest}
+            </SingleInterestText>
+          ))}
+        </InterstContainer>
+        <DropDownAns
+          onClick={() => setIsUserTypeDropdownOpen(!isUserTypeDropdownOpen)}
+        >
+          <GenderText>{userType}</GenderText>
+          {isUserTypeDropdownOpen ? <UpArrowIcon /> : <DownArrowIcon />}
+        </DropDownAns>
+        {isUserTypeDropdownOpen && (
+          <DropDownMainContainer>
+            {userDropdownList.map((feed) => (
+              <DropDownOption
+                onClick={() => {
+                  setUserType(feed);
+                  setIsUserTypeDropdownOpen(false);
+                }}
+              >
+                <DropDownOptionText>{feed}</DropDownOptionText>
+                {userType === feed && <TickIcon />}
+              </DropDownOption>
+            ))}
+          </DropDownMainContainer>
+        )}
+        <BtnConfirm onClick={handleSubmit}>Confirm</BtnConfirm>
+      </ModalMainContainer>
     </>
-  )
+  );
 }
 
-export default InterestModal
+export default InterestModal;
--- frontend/src/pages/Onboard/SignUp.tsx
+++ frontend/src/pages/Onboard/SignUp.tsx
@@ -9,134 +9,154 @@ import InterestModal from './InterestModal';
 import Toaster from '../../common/Toaster';
 
 const Container = styled.div`
-height:100vh;
-width:100%;
-display: flex;
-
+  height: 100vh;
+  width: 100%;
+  display: flex;
 `;
 
 const LeftContainer = styled.div`
-width:50%;
-position: relative;
+  width: 50%;
+  position: relative;
 `;
 
 const RightContainer = styled.div`
-width:50%;
-height: 100%;
-background: #9E4CDC;
-display: flex;
-align-items: center;
-justify-content: center;
+  width: 50%;
+  height: 100%;
+  background: #9e4cdc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Header = styled.div`
-  padding:56px 0 0 95px;
-width:fit-content;
+  padding: 56px 0 0 95px;
+  width: fit-content;
 `;
 
 const Logo = styled.div(({ theme }) => ({
-	...appTypography.h5.bold,
-	color: theme.app.typography['900'],
-	width: 'fit-content',
-	cursor: 'pointer'
+  ...appTypography.h5.bold,
+  color: theme.app.typography['900'],
+  width: 'fit-content',
+  cursor: 'pointer',
 }));
 
 const MainContent = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	gap:54px;
-	flex-direction: column;
-	position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 54px;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const LoginText = styled.p(({ theme }) => ({
-	...appTypography.h1.bold,
-	color: theme.app.typography['900']
+  ...appTypography.h1.bold,
+  color: theme.app.typography['900'],
 }));
 
 const CatContainer = styled.div`
-background: #FFF8E1;
-border-radius: 300px;	
-height:280px;
-width:80%;
+  background: #fff8e1;
+  border-radius: 300px;
+  height: 280px;
+  width: 80%;
 `;
 
 const CatImage = styled.img`
-width:100%;
-height:100%;	
+  width: 100%;
+  height: 100%;
 `;
 
 function SignUp() {
-	const dispatch: any = useDispatch();
-	const navigate = useNavigate();
-	function decodeJwtResponse(token: string) {
-		const base64Url = token.split('.')[1];
-		const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-		const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-			return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-		}).join(''));
-		return JSON.parse(jsonPayload);
-	}
+  const dispatch: any = useDispatch();
+  const navigate = useNavigate();
+  function decodeJwtResponse(token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join(''),
+    );
+    return JSON.parse(jsonPayload);
+  }
 
-	useEffect(() => {
-		window.google.accounts.id.initialize({
-			client_id: '6951006710-td58cqht8oke2isd8fl5c9uv2t1r7bka.apps.googleusercontent.com',
-			callback: handleCredentialResponse
-		});
-		window.google.accounts.id.renderButton(
-			document.getElementById('buttonDiv'),
-			{ theme: 'outline', size: 'large', text: 'Continue with', shape: 'pill', logo_alignment: 'center', width: '280', height: 100, outerHeight: 100, innerHeight: 100 }
-		);
-	}, []);
-	async function handleCredentialResponse(response: { credential: string; }) {
+  useEffect(() => {
+    window.google.accounts.id.initialize({
+      client_id:
+        '6951006710-td58cqht8oke2isd8fl5c9uv2t1r7bka.apps.googleusercontent.com',
+      callback: handleCredentialResponse,
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById('buttonDiv'),
+      {
+        theme: 'outline',
+        size: 'large',
+        text: 'Continue with',
+        shape: 'pill',
+        logo_alignment: 'center',
+        width: '280',
+        height: 100,
+        outerHeight: 100,
+        innerHeight: 100,
+      },
+    );
+  }, []);
+  const [interestModal, setInterestModal] = React.useState(false);
+  async function handleCredentialResponse(response: { credential: string }) {
+    const responsePayload = decodeJwtResponse(response.credential);
+    console.log('Encoded JWT ID token: ' + responsePayload);
+    console.log(
+      responsePayload.name,
+      responsePayload.sub,
+      responsePayload.email,
+    );
+    const body = {
+      userFullName: responsePayload.name,
+      socialRefererId: responsePayload.sub,
+      userEmail: responsePayload.email,
+      interests: [],
+    };
+    const res = await dispatch(_onBoard({ body }));
+    console.log(res.payload.data);
 
-		const responsePayload = decodeJwtResponse(response.credential);
-		console.log('Encoded JWT ID token: ' + responsePayload);
-		console.log(responsePayload.name, responsePayload.sub, responsePayload.email,);
-		const body = {
-			userFullName: responsePayload.name,
-			socialRefererId: responsePayload.sub,
-			userEmail: responsePayload.email,
-			'interests': []
-		};
-		const res = await dispatch(_onBoard({ body }));
-		console.log(res);
+    if (res.meta.requestStatus === 'fulfilled') {
+      if (res.payload.data.interests.length === 0) {
+        setInterestModal(true);
+      } else {
+        navigate('/dashboard');
+      }
+    }
+  }
+  function handleHomeRoute() {
+    navigate('/');
+  }
 
-		if (res.meta.requestStatus === 'fulfilled') {
-			navigate('/');//to dashboard
-		}
+  return (
+    <Container>
+      <LeftContainer>
+        <Header>
+          <Logo onClick={handleHomeRoute}>mama.</Logo>
+        </Header>
 
-	}
-
-	function handleHomeRoute() {
-		navigate('/');
-
-	}
-	return (
-		<Container>
-			<LeftContainer>
-				<Header>
-					<Logo onClick={handleHomeRoute}>mama.</Logo>
-				</Header>
-
-				<MainContent>
-					<LoginText>Login</LoginText>
-					<div id="buttonDiv"></div>
-				</MainContent>
-			</LeftContainer>
-			<RightContainer>
-				<CatContainer>
-					<CatImage src={billiBKL} />
-				</CatContainer>
-
-			</RightContainer>
-			<InterestModal/>
-		</Container>
-	);
+        <MainContent>
+          <LoginText>Login</LoginText>
+          <div id="buttonDiv"></div>
+        </MainContent>
+      </LeftContainer>
+      <RightContainer>
+        <CatContainer>
+          <CatImage src={billiBKL} />
+        </CatContainer>
+      </RightContainer>
+      {interestModal && <InterestModal />}
+    </Container>
+  );
 }
 
 export default SignUp;
