bc1685bb206127c001cc5f27c8ce536d5bedfb64
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-04-22 12:37:22 +0530

    showToast action

--- frontend/src/Store/Reducers/onboard.tsx
+++ frontend/src/Store/Reducers/onboard.tsx
@@ -8,9 +8,19 @@ const appdata = createSlice({
 		user:{},
 		status:'idle',
 		JWT:'',
+		toast:{
+			isVisible:false,
+			text:'',
+			timer:3000
+		},
 	},
 	reducers: {
-		
+		showToast: (state, action) => {
+			const { isVisible = true, text, timer = 1000 } = action.payload;
+			state.toast = {
+				isVisible, text, timer
+			};
+		},
 	},
 	extraReducers:(builder)=>{
 		builder.addCase(
