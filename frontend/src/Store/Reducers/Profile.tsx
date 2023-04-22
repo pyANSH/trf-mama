93b2e44a739de5a24d7383600a396e0e24803965
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-04-22 23:36:54 +0530

    add sell document modal

--- frontend/src/Store/Reducers/Profile.tsx
+++ frontend/src/Store/Reducers/Profile.tsx
@@ -1,30 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  interest: [],
+	interest: [],
 };
 
 const profileSlice = createSlice({
-  name: 'profile',
-  initialState,
-  reducers: {
-    initUpdate: (state: any, action: any) => {
-      state.interest = action.payload;
-    },
-    addInterest: (state: any, action: any) => {
-      if (
-        state.interest.findIndex((item: any) => item === action.payload) === -1
-      )
-        state.interest.push(action.payload);
-    },
-    removeInterest: (state: any, action: any) => {
-      if (state.interest.length > 1) {
-        state.interest = state.interest.filter(
-          (item: any, i: number) => i !== action.payload,
-        );
-      }
-    },
-  },
+	name: 'profile',
+	initialState,
+	reducers: {
+		initUpdate: (state: any, action: any) => {
+			state.interest = action.payload;
+		},
+		addInterest: (state: any, action: any) => {
+			if (
+				state.interest.findIndex((item: any) => item === action.payload) === -1
+			)
+				state.interest.push(action.payload);
+		},
+		removeInterest: (state: any, action: any) => {
+			if (state.interest.length > 1) {
+				state.interest = state.interest.filter(
+					(item: any, i: number) => i !== action.payload,
+				);
+			}
+		},
+	},
 });
 
 export const { addInterest, removeInterest, initUpdate } = profileSlice.actions;
--- frontend/src/pages/Dashboard/components/SellDocumentModal.tsx
+++ frontend/src/pages/Dashboard/components/SellDocumentModal.tsx
@@ -2,6 +2,8 @@ import { CaretDown, CaretUp, Check, X } from 'phosphor-react';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { appTypography } from '../../../config/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTags, initTags, removeTags } from '../../../Store/Reducers/notes';
 
 
 const Backdrop = styled.div(({ theme }) => ({
@@ -200,11 +202,40 @@ align-items: center;
 flex-wrap: wrap;
 `;
 
-function SellDocumentModal({setUploadFiles,uploadFiles}:{setUploadFiles:any,uploadFiles:any}) {
+const HeaderContainer =styled.div`
+display: flex;
+justify-content: flex-end;
+width: 100%;
+`;
+
+const CloseContainer =styled.div`
+display: flex;
+gap: 8px;
+background:${({theme})=>theme.app.neutral['800']};
+border-radius: 100px;
+align-items: center;
+padding: 8px;
+cursor: pointer;
+`;
+
+const CloseText =styled.p(({theme})=>({
+	...appTypography.paraMed.regular,
+	color:theme.app.shades.white
+}));
+
+const CloseIconMain =styled(X)(({theme})=>({
+	width:'16px',
+	height:'16px',
+	color:theme.app.shades.white,
+	cursor:'pointer'
+}));
+
+function SellDocumentModal({setUploadFiles,uploadFiles,setIsDetailsModal}:{setUploadFiles:any,uploadFiles:any,setIsDetailsModal:any}) {
 	const [title, setTitle] = useState('');
 	const [description, setDescription] = useState('');
-	const [tag, setTag] = useState<string>('');
-	const [tagArr, setTagArr] = useState([]);
+	const [tag, setTag] = useState<any>('');
+	const tagArr = useSelector((state:any)=>state.notes.tags);
+	const dispatch = useDispatch();
 	const interestList = [
 		'Education 🎓',
 		'Yeeeah, science! ⚗️',
@@ -237,25 +268,32 @@ function SellDocumentModal({setUploadFiles,uploadFiles}:{setUploadFiles:any,uplo
 
 	function handleAddTags() {
 		if(tag.length>0){
-			const tempTagArr:any = tagArr;
-			tempTagArr.push(tag);
-			setTagArr(tempTagArr);
+			dispatch(addTags(tag));
 			setTag('');
 		}else{
 			return;
 		}
 	}
-	function handleDeleteTags(tagName:string) {
-		const tempTagArr:any = tagArr;
-		const arrIndeex=tempTagArr.findIndex((arrVal:string)=>arrVal===tagName);
-		tempTagArr.splice(arrIndeex,1);
-		setTagArr(tempTagArr);
+	function handleDeleteTags(index:any) {
+		dispatch(removeTags(index));
+	}
+
+	function handleCloseDropdown() {
+		dispatch(initTags());
+		setUploadFiles();
+		setIsDetailsModal(false);
 	}
         
 	return (
 		<>
 			<Backdrop />
 			<ModalMainContainer>
+				<HeaderContainer>
+					<CloseContainer onClick={handleCloseDropdown}>
+						<CloseText>Close</CloseText>
+						<CloseIconMain/>
+					</CloseContainer>
+				</HeaderContainer>
 				<InterstContainer>
 					{interestList.map((interest,index) => (
 						<SingleInterestText
@@ -277,12 +315,12 @@ function SellDocumentModal({setUploadFiles,uploadFiles}:{setUploadFiles:any,uplo
 				</TagsAddContainer>
 				{tagArr?.length>0 && <ActiveTagsContainer>
 					{
-						tagArr?.map((tag,index)=>(
+						tagArr?.map((tag:any,index:any)=>(
 							<ActiveTagMainContainer key={index}>
 								<TagText>
 									{tag}
 								</TagText>
-								<CloseIcon onClick={()=>handleDeleteTags(tag)} />
+								<CloseIcon onClick={()=>handleDeleteTags(index)} />
 							</ActiveTagMainContainer>
 						))
 					}
