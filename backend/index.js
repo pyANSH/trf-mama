38b13cb6f680ba807008fccfd41a318e65517c4b
hkrobotics <kumarhemant24jan@gmail.com>
2023-04-23 12:19:50 +0530

    config : socketio

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,43 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+const cors = require('cors');
+
+const { checkKey, checkToken } = require('./middleware/header');
 const userRoute = require('./router/user/userRoute');
 const meetingRoute = require('./router/meeting/meetingRoute');
 const notesRoute = require('./router/notes/notesRoute');
 const searchRoute = require('./router/search/searchRoute');
-const { checkKey, checkToken } = require('./middleware/header');
-var cors = require('cors')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post('/', checkKey, (req, res) => {
-    res.sendStatus(200);
-})
+  res.sendStatus(200);
+});
+
 app.use('/user', checkKey, userRoute);
 app.use('/meeting', checkKey, checkToken, meetingRoute);
 app.use('/notes', checkKey, checkToken, notesRoute);
 app.use('/search', checkKey, checkToken, searchRoute);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
