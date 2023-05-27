4301baef1301f3def5c4599c64fad1fd42701712
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2023-05-27 15:28:49 +0530

    add ratings

--- backend/Model/notesModal.js
+++ backend/Model/notesModal.js
@@ -39,5 +39,10 @@ const notesSchema = mongoose.Schema({
         type: [String],
         required: true,
     },
+    rating: {
+        type: Number,
+        default: 0,
+
+    },
 });
 module.exports = mongoose.model('notes', notesSchema);
